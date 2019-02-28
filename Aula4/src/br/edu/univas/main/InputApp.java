dfe91d8caca9f7be25cebd25081f04a10100ea22
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-02-28 11:19:05 -0300

    Add projectUri

--- Aula4/src/br/edu/univas/main/InputApp.java
+++ Aula4/src/br/edu/univas/main/InputApp.java
@@ -6,42 +6,51 @@ public class InputApp {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
+		// System.out.println("Digite seu nome:");
+		// String name = scan.nextLine();
+		//
+		// System.out.println("Seja bem vindo " + name);
+		//
+		// System.out.println("Digite sua idade:");
+		// int age = scan.nextInt();
+		//
+		// System.out.println("Você tem " + age + " anos!");
+
+		Aluno[] alunos = new Aluno[3];
 		
-//		System.out.println("Digite seu nome:");
-//		String name = scan.nextLine();
-//		
-//		System.out.println("Seja bem vindo " + name);
-//		
-//		System.out.println("Digite sua idade:");
-//		int age = scan.nextInt();
-//		
-//		System.out.println("Você tem " + age + " anos!");
+		for (int i = 0; i < 3; i++) {
+			Aluno aluno1 = new Aluno();
+
+			System.out.println("Digite o nome:");
+			String name = scan.nextLine();
+			aluno1.setName(name);
+
+			System.out.println("Digite sua idade:");
+			int age = scan.nextInt();
+			aluno1.setAge(age);
+			scan.nextLine();
+
+			System.out.println("Digite o CPF:");
+			String cpf = scan.nextLine();
+			aluno1.setCpf(cpf);
+
+			System.out.println("Digite o e-mail:");
+			String email = scan.nextLine();
+			aluno1.setEmail(email);
+
+			alunos[i] = aluno1;
+		}
 		
-		Aluno aluno1 = new Aluno();
+		System.out.println("\n\n:::Alunos Cadastrados:::\n\n");
 		
-		System.out.println("Digite o nome:");
-		String name = scan.nextLine();
-		aluno1.setName(name);
+		for (int i = 0; i < 3; i++) {
+			Aluno aluno1 = alunos[i];
+			System.out.println("Bem vindo " + aluno1.getName() + " " + aluno1.getEmail() + " " + aluno1.getCpf() + " "
+					+ aluno1.getAge());
+		}
 		
-		System.out.println("Digite sua idade:");
-		// CONTINUAR DAQUI
-		
-		
-		System.out.println("Digite o CPF:");
-		String cpf = scan.nextLine();
-		aluno1.setCpf(cpf);
-		
-		System.out.println("Digite o e-mail:");
-		String email = scan.nextLine();
-		aluno1.setEmail(email);
-		
-		System.out.println("Bem vindo "
-						+ aluno1.getName() + " "
-						+ aluno1.getEmail() + " "
-						+ aluno1.getCpf());
-		
-		
-		
+
 	}
-	
+
 }
---
+++ ProjectsUri/src/br/edu/univas/main/Problem1001.java
@@ -0,0 +1,19 @@
+package br.edu.univas.main;
+
+import java.util.Scanner;
+
+public class Problem1001 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int c = a + b;
+		
+		System.out.println("X = " + c);
+		
+	}
+			
+}
